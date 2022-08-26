68a2b47d049357fc3df78903199cbc834b6165ca
Gio Meneses <gio@meneses.dev>
2022-08-26 12:45:32 -0500

    i3 config look fly

--- install.sh
+++ install.sh
@@ -8,3 +8,11 @@ mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
 
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
+
+########
+## i3 ##
+########
+
+# Deletes current config, BEWARE
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
