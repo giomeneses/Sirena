fadd47b84cc42a0aa86a6160c4bcd817369ab1c3
Gio Meneses <gio@meneses.dev>
2022-08-30 21:10:59 -0500

    nice list for easy viewing

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-# custom functions can go here
+function paclist() {
+  # Based on https://bbs.archlinux.org/viewtopic.php?id=93683
+  pacman -Qqe | \
+    xargs -I '{}' \
+      expac "${bold_color}% 20n ${fg_no_bold[white]}%d${reset_color}" '{}'
+}
+
